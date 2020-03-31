c41a436d4c10b0f3f90df837f430a45bb82616c2
naniandrea2000 <andreanani1400@gmail.com>
2020-03-31 11:40:55 +0200

    Aggiunta metodi login service

--- src/app/services/login.service.ts
+++ src/app/services/login.service.ts
@@ -1,11 +1,35 @@
 import { Injectable } from '@angular/core';
 import { User } from '../models/user.intercae';
+import { Router } from '@angular/router';
 
 @Injectable({
   providedIn: 'root'
 })
 export class LoginService {
 
+  private users: User[] = [
+    {email: "email@gmail.com",username: "andrea",password:"123",admin:true},
+    {email: "email@gmail.com",username: "davide",password:"123",admin:true},
+    {email: "email@gmail.com",username: "marco",password:"123",admin:false}
+  ]
 
-  constructor() { }
+  constructor(private router:Router) {}
+
+  accesso(email: string,username: string,password:string): boolean{
+    let controllo=false;
+    this.users.forEach(element => {
+      if(element.email==email &&element.username==username && element.password==password){
+        element.admin === true ? sessionStorage.setItem('privilege','admin') : sessionStorage.setItem('privilege','user');
+        controllo=true;
+      }
+    });
+    return controllo;
+  }
+
+  eseguiLogin(email: string,username: string,password:string){
+    if (this.accesso(email,username,password)) {
+      sessionStorage.setItem('user', username);
+      this.router.navigateByUrl('/list');
+    }
+  }
 }
