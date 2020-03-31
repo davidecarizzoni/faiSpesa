21aac10469db640f8aa208965e1503bdb968cb6e
davidecarizzoni <davidecarizzoni@gmail.com>
2020-03-31 10:29:18 +0200

    add routing

--- src/app/app-routing-module.ts
+++ src/app/app-routing-module.ts
@@ -2,11 +2,13 @@ import { NgModule } from "@angular/core";
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
 import { LoginComponent } from './components/login/login.component';
+import { ListComponent } from './components/list/list.component';
 
 // DICHIARAZIONE ROUTES DEI COMPONENTI
 const appRoutes: Routes = [
     {path:'home', component: HomeComponent},
-    {path:'login',component: LoginComponent}
+    {path:'login',component: LoginComponent},
+    {path:'list', component: ListComponent}
 ]
 
 @NgModule({
