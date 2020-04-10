3cdcc51ce8d7879a36b830bae2589c6b3e93deeb
davidecarizzoni <davidecarizzoni@gmail.com>
2020-04-10 13:39:59 +0200

    add prodotti

--- src/app/models/lista.interface.ts
+++ src/app/models/lista.interface.ts
@@ -1,5 +1,7 @@
 import { Prodotto } from './prodotto.interface';
 
+
+
 export interface Lista {
     nome: string;
     prodotti: Prodotto[];
---
+++ src/app/services/prodotti.service.ts
@@ -0,0 +1,25 @@
+import { Injectable } from '@angular/core';
+import { Prodotto } from '../models/prodotto.interface';
+import { AngularFireList, AngularFireDatabase } from '@angular/fire/database';
+import { Router } from '@angular/router';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ProdottiService {
+
+  private prodotti: Prodotto[] = [
+      {nome: 'RedBull', descrizione:'Red Bull è una bevanda energetica analcolica di colore ambrato brillante, con taurina e caffeina, è gassata ed ha un gusto leggermente dolce fruttato tendente alla fragola.', prezzo:4, img:''},
+      {nome: 'Riso basmati', descrizione:'Il riso basmati è una varietà di riso a chicco lungo dal sapore caratteristico e molto profumata, adatta a preparare piatti di ispirazione etnica e non solo', prezzo:2, img:''},
+      {nome: 'Penne integrali barilla', descrizione:'Le Penne Rigate Integrali Barilla, dal diametro ridotto e fonte naturale di fibre, si caratterizzano per la loro proverbiale capacità di trattenere il condimento.', prezzo:1, img:''},
+      {nome: 'Hamburger', descrizione:'Polpetta di carne macinata, compressa a formare una specie di medaglione, che si mangia cotta alla griglia o in padella, spesso dentro un panino.', prezzo:5, img:''},
+      {nome: 'I rustici Giovanni Rana', descrizione:'La linea di pasta ripiena in cui ritrovi tutto il piacere della tradizione. Grazie a SFOGLIAGREZZA, il processo brevettato da Giovanni Rana, la pasta è corposa e ruvida per trattenere al meglio il sugo.', prezzo:1.50, img:''},
+      {nome: 'Parmigiano reggiano', descrizione:'Il Parmigiano Reggiano è un formaggio a pasta dura DOP, prodotto con latte vaccino crudo, parzialmente scremato per affioramento, senza l aggiunta di additivi', prezzo:8, img:''},
+  ]
+
+  itemsRef: AngularFireList<Prodotto> = null;
+
+  constructor(private router:Router,public db:AngularFireDatabase) {
+    this.itemsRef=db.list('/prodotti');
+  }
+}
