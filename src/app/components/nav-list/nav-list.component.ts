1dc05ce046a07f54c357917ff5ce07554a77a1d4
naniandrea2000 <andreanani1400@gmail.com>
2020-04-21 10:46:59 +0200

    Add nav bar alla list

--- src/app/components/nav-list/nav-list.component.ts
+++ src/app/components/nav-list/nav-list.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-nav-list',
   templateUrl: './nav-list.component.html',
-  styleUrls: ['./nav-list.component.css']
+  styleUrls: ['./nav-list.component.scss']
 })
 export class NavListComponent implements OnInit {
 
